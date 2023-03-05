9abe7d34f4791d4a495d0a59c2de511a1a38bd28
Valentin <valentin.mudriak@gmail.com>
2023-03-05 19:43:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+function deepCount(a) {
+    let count = a.length;
+    console.log(count);
+    for (let i = 0; i < a.length; i++) {
+        if (Array.isArray(a[i])) {
+            count += deepCount(a[i]);
+        }
+    }
+    return count;
+}
+console.log(deepCount([1, 2, [3, 4, [5]]]));
